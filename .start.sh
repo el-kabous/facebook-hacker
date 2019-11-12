b963a5571b033b3a2dc037b90f802e5c49fb5100
el-kabous <54554756+el-kabous@users.noreply.github.com>
2019-11-12 05:31:15 -0500

    Add files via upload

---
+++ .start.sh
@@ -0,0 +1,75 @@
+RED='\033[0;31m'
+
+clear
+echo ""
+
+echo " [#] STARTING.. " | lolcat -F 20
+echo ""
+sleep 2.5
+echo " [#] PLEASE WAIT.." | lolcat -F 20
+echo ""
+sleep 3.5
+clear
+figlet "facebook" | lolcat -F 20
+sleep 1
+figlet "    hacking" | lolcat -F 20
+sleep 2
+clear
+sleep 1
+echo ""
+echo ""
+read -p $'\e[32m  [#] PLEASE ENTAR THE FACEBOOK ACOUNT (*ID*) :==>> \e[0m ' id
+echo ""
+read -p $'\e[32m  [#] PLEASE ENTER ACOUNT (*NAME*) :==>> \e[0m ' name
+echo ""
+sleep 3
+clear
+sleep 1
+echo ""
+echo ""
+echo " [#] SEARCHING ABOUT :==>> " $id  | lolcat -F 20
+echo ""
+sleep 2
+echo " [#] SEARCHING........" | lolcat -F 20
+echo ""
+sleep 1
+echo " [#] SEARCHING......." | lolcat -F 20
+echo ""
+sleep 1
+echo " [#] SEARCHING......" | lolcat -F 20
+echo ""
+sleep 1
+echo " [#] SEARCHING....." | lolcat -F 20
+echo ""
+sleep 1
+echo " [#] SEARCHING...." | lolcat -F 20
+echo ""
+sleep 1
+echo " [#] SEARCHING..." | lolcat -F 20
+echo ""
+sleep 1
+sleep 0.5
+echo " [#] DONE.." | lolcat -F 20
+echo ""
+sleep 4
+clear
+sleep 1
+echo ""
+echo ""
+echo " [#] ACOUNT ID IS :==>> " $id | lolcat -F 20
+echo ""
+sleep 2
+echo " [#] ACOUNT NAME IS :==>> " $name | lolcat -F 20
+echo ""
+sleep 4
+clear
+sleep 1
+echo ""
+echo ""
+echo " [#] SEARCHING ABOUS PASSWORD ACOUNT..." | lolcat -F 20
+echo ""
+sleep 1.5
+echo " [#] PLEASE WAIT.." | lolcat -F 20
+sleep 9
+clear
+
---
+++ install.sh
@@ -0,0 +1,18 @@
+clear
+python .ELKABOUS.py
+rm -rif .ELKABOUS.py
+mv .start.sh start.sh
+clear
+chmod +x *
+apt install lolcat -y
+clear
+gem install lolcat
+clear
+apt install python python2 -y
+clear
+echo "#this tool made by <ELKABOUS>" | lolcat -F 20
+sleep 3.5
+echo "DONE..." | lolcat -F 20
+sleep 7.5
+clear
+rm -rif install.sh
